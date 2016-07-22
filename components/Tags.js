1ffc1b3e2cfa9d9d7414bdf66f0da183123f6129
David Konsumer <konsumer@jetboystudio.com>
2016-07-22 14:00:41 -0700

    prefix tag link

--- components/Tags.js
+++ components/Tags.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router'
 import {tagMap} from 'utils'
+import { prefixLink } from 'gatsby-helpers'
 
 const Tags = props => {
   const { post, ...rest } = props
@@ -8,7 +9,7 @@ const Tags = props => {
     <div className="Tags" {...rest}>
       {(props.post.tags || []).map((tag, i) => {
          return [i !== 0 ? ' | ' : null,
-           <Link key={i} to={{pathname:'/tags/', hash: '#'+tagMap(tag)}}>
+           <Link key={i} to={{pathname:prefixLink('/tags/'), hash: '#'+tagMap(tag)}}>
              {tag}
            </Link>]
        })}
